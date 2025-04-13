e3417fd4a859c14a085de82c7d6e8f1e56b1a8c9
Gurpreet Singh <103951591+Igurprxxt@users.noreply.github.com>
2025-04-13 16:40:25 +0530

    updated code

--- routes/api/Post.route.js
+++ routes/api/Post.route.js
@@ -6,12 +6,13 @@ const getSinglePost = require("../../controllers/post/getSinglePost");
 const getAdsPost = require("../../controllers/post/getAdsPost");
 const updatePost = require("../../controllers/post/updatePost");
 const updatePostStatus = require("../../controllers/post/updatePostStatus");
+const jwtValidation = require("../../middlewares/jwt_validation");
 
-router.post("/", addPost);
+router.post("/", jwtValidation, addPost);
 router.get("/", getPosts);
 router.get("/list", getAdsPost);
-router.get("/:id", getSinglePost);
-router.put("/updateStatus/:id", updatePostStatus);
-router.put("/updatePost/:id", updatePost);
+router.get("/:id", jwtValidation, getSinglePost);
+router.put("/updateStatus/:id", jwtValidation, updatePostStatus);
+router.put("/updatePost/:id", jwtValidation, updatePost);
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -22,7 +22,7 @@ router.use(
   jwtValidation,
   require("./SubCategory.route")
 );
-router.use("/post", jwtValidation, require("./Post.route"));
+router.use("/post", require("./Post.route"));
 router.use("/conversation", jwtValidation, require("./Conversation.route"));
 
 module.exports = router;
