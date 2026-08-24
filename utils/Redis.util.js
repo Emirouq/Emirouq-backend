03571a04f7c93379733ad14954be814d67ed38b5
GURPREET SINGH <GURPREETSINGH1984RICHI@GMAIL.COM>
2026-08-24 17:55:05 +0530

    committt

--- utils/Redis.util.js
+++ utils/Redis.util.js
@@ -1,16 +1,16 @@
-// const { createClient } = require("redis");
-// const { redis } = require("../config/keys");
+const { createClient } = require("redis");
+const { redis } = require("../config/keys");
 
-// let redisClient;
+let redisClient;
 
-// (async () => {
-//   redisClient = createClient({
-//     url: redis.url,
-//   });
-//   await redisClient.connect();
-//   redisClient.on("error", function (error) {
-//     console.error("Error in redis connect: ", error);
-//   });
-// })();
+(async () => {
+  redisClient = createClient({
+    url: redis.url,
+  });
+  await redisClient.connect();
+  redisClient.on("error", function (error) {
+    console.error("Error in redis connect: ", error);
+  });
+})();
 
-// module.exports = redisClient;
+module.exports = redisClient;
